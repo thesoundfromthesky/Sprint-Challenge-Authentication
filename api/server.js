fd93e1f6502781c600491ed680698ce14d78659d
thesoundfromthesky <nagara@naver.com>
2020-01-31 10:30:24 -0600

    register and login

--- api/server.js
+++ api/server.js
@@ -1,18 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 
-const authenticate = require('../auth/authenticate-middleware.js');
-const authRouter = require('../auth/auth-router.js');
-const jokesRouter = require('../jokes/jokes-router.js');
+const authenticate = require("../auth/authenticate-middleware.js");
+const authRouter = require("../auth/auth-router.js");
+const jokesRouter = require("../jokes/jokes-router.js");
 
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
 
-server.use('/api/auth', authRouter);
-server.use('/api/jokes', authenticate, jokesRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/jokes", authenticate, jokesRouter);
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,58 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const db = require("../database/dbConfig");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
+// - [ ] Implement the `register` and `login` functionality inside `/auth/auth-router.js`.
+// A `user` has `username` and `password`. Both properties are required.
+router.post("/register", async (req, res) => {
   // implement registration
+  try {
+    req.body.password = bcryptjs.hashSync(req.body.password);
+    const isCreated = await db("users").insert(req.body);
+    if (isCreated) {
+      res.status(201).json({
+        statusCode: 201,
+        message: "Create Success"
+      });
+    } else {
+      res.status(201).json({
+        statusCode: 201,
+        message: "Create Fail"
+      });
+    }
+  } catch {
+    internalServerError(res);
+  }
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", async (req, res) => {
   // implement login
+  try {
+    const { username, password } = req.body;
+    const [user] = await db("users").where({ username });
+    if (user && bcryptjs.compareSync(password, user.password)) {
+      const payload = { username };
+      const options = { expiresIn: "1d" };
+      const token = jwt.sign(payload, process.env.JWT_SECRET, options);
+      if (token) {
+        res.status(200).json({ token });
+      } else {
+        throw new Error();
+      }
+    } else {
+      throw new Error();
+    }
+  } catch {
+    internalServerError(res);
+  }
 });
 
+function internalServerError(res) {
+  res.status(500).json({
+    statusCode: 500,
+    error: "Internal Server Error",
+    message: "Error with register"
+  });
+}
 module.exports = router;
